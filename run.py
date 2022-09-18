84bf1bfea5b3666745362dea32cc71292ba395db
nyeincz <nyeincz@gmail.com>
2022-09-19 02:35:56 +0800

    print custom message after app started

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from application import create_app
+import sys
+
+print('Rainfall Service Started', file=sys.stdout)
 app = create_app()
 app.run(
      host='0.0.0.0',
      port='8080',
      debug='True'
 )
-

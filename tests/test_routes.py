271184b7db7cd45e1b0fd8a12f003094aca5cc96
nyeincz <nyeincz@gmail.com>
2022-09-19 01:36:39 +0800

    testing app with pytest

---
+++ tests/test_routes.py
@@ -0,0 +1,5 @@
+# def test_request_example(client):
+#     response = client.get("/")
+#     assert b"Rainig" in response.data
+# def test_raindata(client, app):
+#     assert client.get("/").status_code == 200
